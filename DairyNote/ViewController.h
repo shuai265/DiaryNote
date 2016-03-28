cea0f7a5d4983074f25086fdb3b49160e509bb7f
shuai265 <shuai265@qq.com>
2016-03-28 12:51:45 +0800

    Initial Commit

---
+++ DairyNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DairyNote
+//
+//  Created by 刘帅 on 3/28/16.
+//  Copyright © 2016 刘帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DairyNote/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DairyNote
+//
+//  Created by 刘帅 on 3/28/16.
+//  Copyright © 2016 刘帅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
